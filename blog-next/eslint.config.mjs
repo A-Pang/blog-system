32460c7db8690e08de309d21ae043178221fd844
peng-zai <ppee13790107530>
2025-10-30 20:51:45 +0800

    update

--- blog-next/eslint.config.mjs
+++ blog-next/eslint.config.mjs
@@ -1,8 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import nextVitals from 'eslint-config-next/core-web-vitals'
-import { globalIgnores } from 'eslint/config'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,23 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...nextVitals,
-  {
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
-  },
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
-
-
 ];
 
 export default eslintConfig;
